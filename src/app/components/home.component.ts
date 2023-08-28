369f571d4e303d4a064a99b90e2cdd3c70018a2a
isumannag <tuition.suman.nag@gmail.com>
2023-08-28 12:00:42 +0530

    Component Selector for Angular

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home',       // By Element Itself (default)
+  // selector: '[app-home]',  // By Attribute
+  // selector: '.app-home',   // By class
   template: `
   <p><b>Below are the list of things we have created using {{name}}:</b></p>
   <p>1. ng new my-first-project <br>
@@ -11,7 +13,11 @@ import { Component } from '@angular/core';
      "styles": [
       "node_modules/bootstrap/dist/css/bootstrap.min.css",
       "src/styles.css"
-      ]
+      ] <br>
+     4. Component Selector in Angular <br> 
+      selector: 'app-home' (Default Element Selector)<br> 
+      selector: '[app-home]' (Attribute Selector)<br> 
+      selector: '.app-home', (Class Selector)<br> 
   </p>
 
   `,
