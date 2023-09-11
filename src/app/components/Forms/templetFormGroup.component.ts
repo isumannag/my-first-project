78523d10b10af49969a5d3da435000596dbb8a8b
isumannag <tuition.suman.nag@gmail.com>
2023-09-11 16:24:36 +0530

    Templet driven form

--- src/app/components/Forms/templetFormGroup.component.ts
+++ src/app/components/Forms/templetFormGroup.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 @Component({
     selector: 'app-myapp',
     template: `
         <h1>Templet Driven Form Example</h1>
-        <form #ff="ngForm" (ngSubmit)="registerMyUser(ff)">
+        <form #ff='ngForm' (ngSubmit)="registerMyUser()">
             <div ngModelGroup="allDataForm" #gg="ngModelGroup">
                 <label>Email</label>
                 <input type="text" name="nameVal" minlength="4" maxlength="10" ngModel required email><br>
@@ -19,18 +19,40 @@ import { NgForm } from "@angular/forms";
                 <input type="radio" name="gender" ngModel [value]="gender" required> {{gender}}
             </div><br><br>
 
-            <button type="submit" class="btn btn-primary" [disabled]="!ff.valid">Sign Up</button>
-        </form>
+            <button type="submit" class="btn btn-primary" >Sign Up</button>
+            <button type="submit" class="btn btn-secondary" (click)="resetMyForm()">Reset Form</button>
+        </form><br>
+        <button type="submit" class="btn btn-secondary" (click)="setFormValues()">Set Form Data using setValue property</button>
     `,
     styles: [`input.ng-invalid.ng-touched {border:1px solid red;}`]
 })
 export class TempletFromGroupComponent{
+    @ViewChild('ff') allformData: NgForm; // Using view change also we can retrive form data
     genders = ['male', 'female'];
-    registerMyUser(myNgForm: NgForm){
+    registerMyUser(){
         alert(
-        "Success!!  Username: "+myNgForm.value.allDataForm.nameVal
-        +' Password: '+myNgForm.value.allDataForm.password
-        +' Secret Question: '+myNgForm.value.gender
+        "Success!!  Username: "+this.allformData.value.allDataForm.nameVal
+        +' Password: '+this.allformData.value.allDataForm.password
+        +' Secret Question: '+this.allformData.value.gender
         );
+    }
+
+    // Form input vale set by two different method setValue & patchValue
+    setFormValues(){
+        const defaultVal = {
+            allDataForm: {
+                nameVal: 'sn@g.c',
+                password: 'sd'
+            },
+            gender: 'female'
+        };
+
+        // this.allformData.setValue(defaultVal);
+        this.allformData.form.patchValue({gender:'female'});
+    }
+
+    // Reset form using reset method
+    resetMyForm(){
+        this.allformData.reset();
     }
 }
